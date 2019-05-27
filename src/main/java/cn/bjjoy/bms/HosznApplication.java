ba9a9670fbd8bf0d77b8d641f1382a985181d472
BeckXu0089124 <Zhijie.Xu@ge.com>
2019-05-27 23:38:03 +0800

    1, redesign socket 2, change password 3, other issues

--- src/main/java/cn/bjjoy/bms/HosznApplication.java
+++ src/main/java/cn/bjjoy/bms/HosznApplication.java
@@ -6,10 +6,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import cn.bjjoy.bms.setting.constants.Constants;
 import cn.bjjoy.bms.setting.controller.SingletonSocket;
-import cn.bjjoy.bms.socket_multi_nio.NIOSServer8082;
-import cn.bjjoy.bms.socket_multi_nio.NIOSServer8084;
+import cn.bjjoy.bms.socket_multi_nio.HosznServer8082;
+import cn.bjjoy.bms.socket_multi_nio.HosznServer8084;
 
 
 @SpringBootApplication
@@ -17,16 +16,27 @@ import cn.bjjoy.bms.socket_multi_nio.NIOSServer8084;
 @EnableAsync
 @EnableScheduling
 public class BmsApplication {
-
+	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(BmsApplication.class, args);
 		
+		int[] ports = {8082,8084};
+		
 		new Thread(new Runnable() {
 			public void run() {
 				try {
-					new NIOSServer8082(Constants.SOCKET_8082_PORT).listen() ;
-					new NIOSServer8084(Constants.SOCKET_8084_PORT).listen() ;
+					new HosznServer8084().listen(ports[1]);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					new HosznServer8082().listen(ports[0]);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
